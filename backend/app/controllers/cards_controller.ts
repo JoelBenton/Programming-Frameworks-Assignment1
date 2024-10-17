ed4656f09bc080766518d59926146ffff25beda0
JoelBenton <joelbenton1903@gmail.com>
2024-10-17 22:04:08 +0100

    Remove Auth pre made setup, Created controllers and setup routes

---
+++ backend/app/controllers/cards_controller.ts
@@ -0,0 +1,8 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class CardsController {
+  /**
+   * Display a list of resource
+   */
+  async index({ params, response }: HttpContext) {}
+}
---
+++ backend/app/controllers/sets_controller.ts
@@ -0,0 +1,38 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class SetsController {
+  /**
+   * Display a list of resource
+   */
+  async index({}: HttpContext) {}
+
+  /**
+   * Display a list of resource for user ID.
+   */
+  async userIndex({ params, response }: HttpContext) {}
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request, response }: HttpContext) {}
+
+  /**
+   * Show individual record
+   */
+  async show({ params, response }: HttpContext) {}
+
+  /**
+   * Edit individual record
+   */
+  async edit({ params, response }: HttpContext) {}
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request, response }: HttpContext) {}
+
+  /**
+   * Delete record
+   */
+  async destroy({ params, response }: HttpContext) {}
+}
