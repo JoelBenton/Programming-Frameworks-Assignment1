9daf90f0ae668ca8ef1ddfa7dfc017cd15cc37a9
JoelBenton <joelbenton1903@gmail.com>
2024-10-22 14:20:01 +0100

    Setup Basic Username and password authentication using Tokens

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -9,7 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 import { throttle } from './limiter.js'
-import Redis from '@adonisjs/redis/services/main'
+import { middleware } from './kernel.js'
 
 // Dynamic imports for HMR
 const SetsController = () => import('#controllers/sets_controller')
@@ -19,8 +19,7 @@ const CardsController = () => import('#controllers/cards_controller')
 const CollectionsController = () => import('#controllers/collections_controller')
 const UserCollectionsController = () => import('#controllers/user_collections_controller')
 const LimitsController = () => import('#controllers/limits_controller')
-
-const { default: limitController } = await LimitsController()
+const AuthController = () => import('#controllers/auth_controller')
 
 // Api Router Group
 router
@@ -30,54 +29,36 @@ router
         hello: 'world',
       }
     })
+    router.post('/users/register', [AuthController, 'register'])
+    router.post('/users/login', [AuthController, 'login'])
+    router.post('/users/logout', [AuthController, 'logout']).use(middleware.auth())
 
-    // Dynamic import for each route
-    router.get('/users/:user_id/sets', async (ctx) => {
-      const { default: SetController } = await SetsController()
-      return new SetController().userIndex(ctx)
-    })
-
-    router.get('/collections/random', async (ctx) => {
-      const { default: CollectionController } = await CollectionsController()
-      return new CollectionController().indexRandom(ctx)
-    })
-
-    router.post('/users', async (ctx) => {
-      const { default: UserController } = await UsersController()
-      return new UserController().store(ctx)
-    })
-
+    router.get('/users/:user_id/sets', [SetsController, 'userIndex']).use(middleware.auth())
+    router.get('/collections/random', [CollectionsController, 'indexRandom']).use(middleware.auth())
+    router.post('/users', [UsersController, 'store']).use(middleware.auth())
+    router.post('/sets', [SetsController, 'store']).use(throttle).use(middleware.auth())
+    router.post('sets/update-limit', [LimitsController, 'updateLimit']).use(middleware.auth())
+    router.post('sets/get-limit', [LimitsController, 'getLimit']).use(middleware.auth())
     router
-      .post('/sets', async (ctx) => {
-        const { default: SetController } = await SetsController()
-        return new SetController().store(ctx)
-      })
-      .use(throttle)
+      .post('sets/get-remaining-requests', [LimitsController, 'getRemainingRequests'])
+      .use(middleware.auth())
 
-    router.post('sets/update-limit', async (ctx) => {
-      return new limitController().updateLimit(ctx)
-    })
-
-    router.post('sets/get-limit', async (ctx) => {
-      return new limitController().getLimit(ctx)
-    })
-
-    router.post('sets/get-remaining-requests', async (ctx) => {
-      return new limitController().getRemainingRequests(ctx)
-    })
-
-    // Use dynamic imports with router.resource API
-    router.resource('sets', SetsController).apiOnly().except(['store'])
-    router.resource('set.comment', CommentsController).only(['store'])
-    router.resource('sets.cards', CardsController).only(['index'])
+    router.resource('sets', SetsController).apiOnly().except(['store']).use('*', middleware.auth())
+    router.resource('set.comment', CommentsController).only(['store']).use('*', middleware.auth())
+    router.resource('sets.cards', CardsController).only(['index']).use('*', middleware.auth())
     router
       .resource('users', UsersController)
       .apiOnly()
       .except(['store'])
       .where('id', router.matchers.number())
-    router.resource('collections', CollectionsController).only(['index', 'store'])
+      .use('*', middleware.auth())
+    router
+      .resource('collections', CollectionsController)
+      .only(['index', 'store'])
+      .use('*', middleware.auth())
     router
       .resource('users.collections', UserCollectionsController)
       .only(['index', 'show', 'update', 'destroy'])
+      .use('*', middleware.auth())
   })
   .prefix('/api')
